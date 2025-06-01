df9112166bda186baa1aabaf6b0d2681b5956f6d
Ahmed Maged <mageda9000@gmail.com>
2025-06-02 00:10:08 +0300

    Update CMake configuration: remove unnecessary includes, add GoogleTest dependency, and adjust target link libraries for unit tests

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -11,3 +11,15 @@ set(ThreadPool_BUILD_EXAMPLES OFF CACHE BOOL "Disable examples" FORCE)
 set(ThreadPool_BUILD_Doc OFF CACHE BOOL "Disable doc" FORCE)
 
 FetchContent_MakeAvailable(thread_pool)
+
+####################################### Fetch googletest #######################################
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG        v1.15.0
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
+
+include(GoogleTest)
