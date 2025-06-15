c647f2e2b0a28f528ae8f3fe42202db5460b5b3b
Ahmed Maged <mageda9000@gmail.com>
2025-06-16 00:41:48 +0300

    Refactor Vector::Sum to use VectorSumBlock_Cuda for final reduction

--- lib/source/vector/Vector_Sum.cu
+++ lib/source/vector/Vector_Sum.cu
@@ -1,4 +1,5 @@
 #include "vector.hpp"
+#include <algorithm>
 
 #define BLOCK_SIZE 256
 
@@ -30,30 +31,54 @@ namespace qlm
         }
     }
 
+    __global__ void VectorSumBlock_Cuda(const float* in, const int length, float* result)
+    {
+        const int tid = threadIdx.x;
+
+        __shared__ float partial_sum[BLOCK_SIZE];
+        partial_sum[tid] = (tid < length) ? in[tid] : 0.0f;
+
+	    __syncthreads();
+
+        for (int s = 1; s < blockDim.x; s *= 2) 
+        {    
+            const int index = 2 * s * tid;
+            if (index < blockDim.x)
+            {
+                partial_sum[index] += partial_sum[index + s];
+            }
+            __syncthreads();
+        }
+
+        if (tid == 0) 
+        {
+            *result += partial_sum[0];
+        }
+    }
+
+
     void qlm::Vector::Sum(DeviceMemory& result) const
 	{
         // Launch kernel
         const int block_size = BLOCK_SIZE;
         const int num_blocks = (length + block_size - 1) / block_size;
+
         // allocate device memory for the result
         float* sum_result;
         cudaMalloc(&sum_result, num_blocks * sizeof(float));
         
+        // First reduction: input -> partial sums
         VectorSum_Cuda<<<num_blocks, block_size>>>(data, length, sum_result);
-        // VectorSum_Cuda<<<1, num_blocks>>>(sum_result, num_blocks, result.data);
+        cudaDeviceSynchronize();
 
-        cudaDeviceSynchronize(); // Ensure the kernel execution is complete
-
-        float res = 0;
-        for (int i = 0; i < num_blocks; ++i) 
+        // Second reduction: partial sums -> final sum
+        for (int i = 0; i < num_blocks; i += block_size) 
         {
-            float block_sum;
-            cudaMemcpy(&block_sum, &sum_result[i], sizeof(float), cudaMemcpyDeviceToHost);
-            res += block_sum;
+            const int cur_length = std::min(block_size, num_blocks - i);
+            VectorSumBlock_Cuda<<<1, block_size>>>(&sum_result[i], cur_length, result.data);
+            cudaDeviceSynchronize();
         }
-        // Copy the result back to the host
-        result.FromCPU(&res); // Copy the final sum to the result device memory
-
+        
         cudaFree(sum_result); // Free the device memory
 	}
 }
