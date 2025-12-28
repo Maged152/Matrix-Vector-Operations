81e590293a6914d36ef1ea06ee661415c2cc6d44
Ahmed Maged <mageda9000@gmail.com>
2025-12-28 20:36:30 +0200

    Refactor VectorSum_Cuda to use atomicAdd for result accumulation and remove unused VectorSumBlock_Cuda kernel

--- lib/source/vector/Vector_Sum.cu
+++ lib/source/vector/Vector_Sum.cu
@@ -41,39 +41,9 @@ namespace qlm
 
         if (tid == 0) 
         {
-            result[blockIdx.x] = partial_sum[0];
+            atomicAdd(result, partial_sum[0]);
         }
     }
-
-    __global__ void VectorSumBlock_Cuda(const float* in, const int length, float* result)
-    {
-        const int tid = threadIdx.x;
-
-        __shared__ float partial_sum[BLOCK_SIZE];
-        partial_sum[tid] = (tid < length) ? in[tid] : 0.0f;
-
-	    __syncthreads();
-
-        for (int s = blockDim.x / 2; s > 32; s >>= 1) 
-        {
-            if (tid < s)
-            {
-                partial_sum[tid] += partial_sum[tid + s];
-            }
-            __syncthreads();
-        }
-
-        // last warp
-        if (tid < 32) 
-            WarpReduce(partial_sum, tid);
-
-
-        if (tid == 0) 
-        {
-            *result += partial_sum[0];
-        }
-    }
-
 
     void qlm::Sum(const Vector &src, DeviceFloat& result)
 	{
@@ -81,23 +51,8 @@ namespace qlm
         // Launch kernel
         const int block_size = BLOCK_SIZE;
         const int num_blocks = (length + (block_size * 2) - 1) / (block_size * 2);
-
-        // allocate device memory for the result
-        float* sum_result;
-        cudaMalloc(&sum_result, num_blocks * sizeof(float));
         
-        // First reduction: input -> partial sums
-        VectorSum_Cuda<<<num_blocks, block_size>>>(src.data, length, sum_result);
+        VectorSum_Cuda<<<num_blocks, block_size>>>(src.data, length, result.mem.data);
         cudaDeviceSynchronize();
-
-        // Second reduction: partial sums -> final sum
-        for (int i = 0; i < num_blocks; i += block_size) 
-        {
-            const int cur_length = std::min(block_size, num_blocks - i);
-            VectorSumBlock_Cuda<<<1, block_size>>>(&sum_result[i], cur_length, result.mem.data);
-            cudaDeviceSynchronize();
-        }
-        
-        cudaFree(sum_result); // Free the device memory
 	}
 }
