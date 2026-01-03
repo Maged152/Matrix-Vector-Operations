324eb15108096fb9553be172ef74a5dea784f351
Ahmed Maged <mageda9000@gmail.com>
2026-01-03 21:03:53 +0200

    warp shuffle

--- lib/source/vector/Vector_Sum.cu
+++ lib/source/vector/Vector_Sum.cu
@@ -44,6 +44,48 @@ namespace qlm
             atomicAdd(result, partial_sum[0]);
         }
     }
+    /*
+     warp shuffle
+      __global__ void VectorSum_Cuda(const float* in, const int length, float* result)
+    {
+        const int tid = threadIdx.x;
+        const int gid = blockIdx.x * blockDim.x * 2 + tid;
+
+        const int warp_id = tid / WARP_SIZE;
+        const int lane = tid % WARP_SIZE;
+
+        __shared__ float partial_sum[WARP_SIZE];
+
+        // Load elements & do first add of reduction
+        const float second_element = (gid + blockDim.x < length) ? in[gid + blockDim.x] : 0.0f;
+        float val = in[gid] + second_element;
+
+        for (int s = WARP_SIZE / 2; s > 0; s >>= 1) 
+        { 
+            val += __shfl_down_sync(0xFFFFFFFF, val, s);
+        }
+
+        if (lane == 0) partial_sum[warp_id] = val;
+
+        __syncthreads();
+
+        if (warp_id == 0) 
+        {
+            // reload val from shared memory
+           val = (tid < (blockDim.x / WARP_SIZE)) ? partial_sum[tid] : 0.0f;
+
+            for (int s = WARP_SIZE / 2; s > 0; s >>= 1) 
+            { 
+                val += __shfl_down_sync(0xFFFFFFFF, val, s);
+            }
+
+            if (tid == 0) 
+            {
+                atomicAdd(result, val);
+            }
+        }
+    }
+    */
 
     void qlm::Sum(const Vector &src, DeviceFloat& result)
 	{
